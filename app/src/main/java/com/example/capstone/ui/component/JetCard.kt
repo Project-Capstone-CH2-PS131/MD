4871949cd035f0b8c144895436c70b8047a1e8e4
Rafli Zufargian <raflizufargian@gmail.com>
2023-11-28 21:19:16 +0700

    Clean Conflict

--- app/src/main/java/com/example/capstone/ui/component/JetCard.kt
+++ app/src/main/java/com/example/capstone/ui/component/JetCard.kt
@@ -1,9 +1,12 @@
 package com.example.capstone.ui.component
 
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 
 
 @Composable
 fun JetCard() {
-
+    Card() {
+        
+    }
 }
