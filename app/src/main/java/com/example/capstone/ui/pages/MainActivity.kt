66e08f454d581a9861c09d60b5e52bb3ea135f4d
BosToken <faizdiandra11@gmail.com>
2023-11-28 11:03:54 +0700

    4 ui theme color

--- app/src/main/java/com/example/capstone/ui/pages/MainActivity.kt
+++ app/src/main/java/com/example/capstone/ui/pages/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.capstone.ui.pages
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.capstone.ui.component.JetTextField
 import com.example.capstone.ui.loginpage.LoginPage
 import com.example.capstone.ui.theme.CapstoneTheme
 
@@ -18,7 +20,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CapstoneTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -33,6 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting() {
     LoginPage()
+//    WelcomePage()
 }
 
 @Preview(showBackground = true)
