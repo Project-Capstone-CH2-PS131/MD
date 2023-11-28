ae72e610a22fd02159c345338e36d5f5a5b0bc46
Rafli Zufargian <raflizufargian@gmail.com>
2023-11-29 02:29:31 +0700

    Card Fridge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
 
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
