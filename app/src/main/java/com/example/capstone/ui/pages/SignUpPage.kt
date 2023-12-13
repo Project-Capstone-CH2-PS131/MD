a567adba3ebe2897f4cfd924932699d964ec5bc8
BosToken <46486733+BosToken@users.noreply.github.com>
2023-12-13 09:21:21 -0800

    Update SignUpPage.kt

--- app/src/main/java/com/example/capstone/ui/pages/SignUpPage.kt
+++ app/src/main/java/com/example/capstone/ui/pages/SignUpPage.kt
@@ -1,14 +1,9 @@
 package com.example.capstone.ui.pages
 
-import android.graphics.drawable.Icon
-import android.media.JetPlayer
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,29 +11,37 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountBox
 import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.capstone.R
+import com.example.capstone.data.remote.response.AuthResponse
+import com.example.capstone.data.remote.retrofit.ApiConfig
 import com.example.capstone.ui.component.JetButton
 import com.example.capstone.ui.component.JetTextField
 import com.example.capstone.ui.theme.BlackPrimary
 import com.example.capstone.ui.theme.BluePrimary
 import com.example.capstone.ui.theme.BlueText
-
+import retrofit2.Call
+import retrofit2.Callback
 
 @Composable
 fun SignUpPage(){
+    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
             .fillMaxHeight(1f)
@@ -65,12 +68,31 @@ fun SignUpPage(){
             textAlign = TextAlign.Center,
             color = BlackPrimary,
         )
-        JetTextField(hint = "USERNAME", icon = Icons.Outlined.Person, keyboardType = KeyboardType.Text)
+        JetTextField(hint = "USERNAME", icon = Icons.Outlined.Person, keyboardType = KeyboardType.Text, value = { new -> username = new })
         Column(modifier = Modifier.padding(5.dp)){}
-        JetTextField(hint = "EMAIL", icon = Icons.Outlined.Email, keyboardType = KeyboardType.Email)
+        JetTextField(hint = "EMAIL", icon = Icons.Outlined.Email, keyboardType = KeyboardType.Email, value = { new -> email = new })
         Column(modifier = Modifier.padding(5.dp)){}
-        JetTextField(hint = "PASSWORD", icon = Icons.Outlined.AccountBox, keyboardType = KeyboardType.Password)
+        JetTextField(hint = "PASSWORD", icon = Icons.Outlined.AccountBox, keyboardType = KeyboardType.Password, value = { new -> password = new })
         Column(modifier = Modifier.padding(20.dp)){}
-        JetButton(onClick = {}, color = BluePrimary, enabled = true, label = "SIGN UP")
+        JetButton(onClick = {register(username, email, password)}, color = BluePrimary, enabled = true, label = "SIGN UP")
     }
 }
+
+private fun register(username: String, email: String, password: String){
+    val client = ApiConfig.getApiService().register(username, email, password)
+    client.enqueue(object : Callback<AuthResponse>{
+        override fun onResponse(call: Call<AuthResponse>, response: retrofit2.Response<AuthResponse>) {
+            if (response.isSuccessful){
+                val responseBody = response.body()
+                Log.d("test123", "success "+responseBody!!.token)
+            }
+            else{
+            }
+        }
+
+        override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+            Log.d("Error : ", call.toString())
+        }
+    })
+}
+
