908a705c16da17dbd3b8f18fc7a0aedbc6709dea
BosToken <faizdiandra11@gmail.com>
2023-12-13 19:35:30 +0700

    fixing top bar

---
+++ app/src/main/java/com/example/capstone/ui/component/JetTopBar.kt
@@ -0,0 +1,56 @@
+package com.example.capstone.ui.component
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.capstone.ui.theme.CapstoneTheme
+import com.example.capstone.ui.theme.BluePrimary
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun JetTopBar(){
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = BluePrimary,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = {
+
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Localized description"
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Localized description"
+                )
+            }
+        },
+        scrollBehavior = scrollBehavior,
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun JetTopBarPreview(){
+    CapstoneTheme {
+        JetTopBar()
+    }
+}
