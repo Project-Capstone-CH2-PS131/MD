19b0ee13fc1a5224b7d5ebe0e1d276e539fc5f76
BosToken <46486733+BosToken@users.noreply.github.com>
2023-12-13 09:17:00 -0800

    Update AuthResponse.kt

--- app/src/main/java/com/example/capstone/data/remote/response/AuthResponse.kt
+++ app/src/main/java/com/example/capstone/data/remote/response/AuthResponse.kt
@@ -5,8 +5,11 @@ import com.google.gson.annotations.SerializedName
 data class AuthResponse(
 
 	@field:SerializedName("error")
-	val error: Boolean,
+	val error: Boolean? = null,
 
 	@field:SerializedName("message")
-	val message: String
+	val message: String? = null,
+
+	@field:SerializedName("token")
+	val token: String? = null
 )
