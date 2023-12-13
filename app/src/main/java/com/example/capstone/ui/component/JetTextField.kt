cd22ec59142a92230a95956506f1d618456c070b
BosToken <46486733+BosToken@users.noreply.github.com>
2023-12-13 09:19:35 -0800

    Update JetTextField.kt

--- app/src/main/java/com/example/capstone/ui/component/JetTextField.kt
+++ app/src/main/java/com/example/capstone/ui/component/JetTextField.kt
@@ -1,5 +1,6 @@
 package com.example.capstone.ui.component
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,14 +23,15 @@ import androidx.compose.ui.unit.dp
 import com.example.capstone.ui.theme.BluePrimary
 import com.example.capstone.ui.theme.BlueSecondary
 
+@SuppressLint("SuspiciousIndentation")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun JetTextField(
+    value: (String) -> Unit,
     modifier: Modifier = Modifier,
     hint: String,
     icon: ImageVector,
-    keyboardType : KeyboardType,
-    maxline: Int = 1
+    keyboardType : KeyboardType
 ){
     var visual: VisualTransformation = VisualTransformation.None
     if(keyboardType == KeyboardType.Password){
@@ -38,7 +40,10 @@ fun JetTextField(
     var input by remember { mutableStateOf("") }
         TextField(
             value = input,
-            onValueChange = {input = it},
+            onValueChange = {
+                input = it
+                value(it)
+            },
             label = {
                 Text(text = hint)
             },
@@ -60,8 +65,6 @@ fun JetTextField(
                 focusedIndicatorColor = BluePrimary,
                 containerColor = BlueSecondary.copy(.1f)
             ),
-            modifier = modifier,
-            visualTransformation = visual,
-            maxLines = maxline
+            modifier = modifier
         )
 }
