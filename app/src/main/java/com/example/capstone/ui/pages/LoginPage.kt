02cc104eac56e635e1fdc34c7ab8675547e7b29d
BosToken <46486733+BosToken@users.noreply.github.com>
2023-12-13 09:20:26 -0800

    Update LoginPage.kt

--- app/src/main/java/com/example/capstone/ui/pages/LoginPage.kt
+++ app/src/main/java/com/example/capstone/ui/pages/LoginPage.kt
@@ -8,32 +8,34 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AccountBox
-import androidx.compose.material.icons.outlined.Email
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.capstone.R
-import com.example.capstone.ui.component.JetButton
-import com.example.capstone.ui.component.JetTextField
-import com.example.capstone.ui.theme.BluePrimary
+import com.example.capstone.ui.component.SliceButton
+import com.example.capstone.ui.component.TextFieldEmail
+import com.example.capstone.ui.component.TextFieldPassword
 import com.example.capstone.ui.theme.CapstoneTheme
 
 @Composable
 fun LoginPage() {
 
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -57,9 +59,6 @@ fun LoginPage() {
             modifier = Modifier.height(250.dp)
         )
 
-
-
-
         Text(
             text = "LOG IN",
             fontSize = 24.sp,
@@ -74,9 +73,9 @@ fun LoginPage() {
             color = Color(android.graphics.Color.parseColor("#000000")),
             )
 
-        JetTextField(hint = "EMAIL", icon = Icons.Outlined.Email, keyboardType = KeyboardType.Email)
+        JetTextField(hint = "EMAIL", icon = Icons.Outlined.Email, keyboardType = KeyboardType.Email, value = { new -> email = new })
         Column(modifier = Modifier.padding(5.dp)) {}
-        JetTextField(hint = "PASSWORD", icon = Icons.Outlined.AccountBox, keyboardType = KeyboardType.Password)
+        JetTextField(hint = "PASSWORD", icon = Icons.Outlined.AccountBox, keyboardType = KeyboardType.Password, value = { new -> password = new })
         Column(modifier = Modifier.padding(20.dp)) {}
         JetButton(onClick = {}, color = BluePrimary, enabled = true, label = "LOG IN")
     }
